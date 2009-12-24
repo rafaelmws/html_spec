4e95a07d8fcfb6e41dde68c14c1b642c81122033
Rafael <rafael@Grifo.(none)>
2009-12-24 11:29:50 -0200

    first commit

---
+++ html_spec.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import re
+from lxml import etree
+from tag_exceptions import DoesNotHaveTagException
+
+class HtmlSpec(object):
+    html = None
+    tree = None
+    node = None
+
+    def __init__(self, html, node=None):
+        self.html = html
+        self.tree = etree.fromstring(str(html))
+        self.node = node
+        
+        if(self.node == None):
+            self.node = self.tree
+
+    def __find__(self, tag_name, **kwargs):
+        xpath = self.make_xpath(tag_name, **kwargs)
+        return self.tree.find(xpath)
+
+    def has_tag(self, tag_name, **kwargs):
+        self.node = self.__find__(tag_name, **kwargs)
+        
+        if self.node != None:
+            return HtmlSpec(etree.tostring(self.node),
+                                    node = self.node)
+        else:
+            raise DoesNotHaveTagException('Html does not have tag %s' % tag_name)
+    
+    def with_tag(self, tag_name):
+        self.__find__(tag_name)
+
+        if self.node != None:
+            return HtmlSpec(self.html)
+        else:
+            raise DoesNotHaveTagException('Html does not have tag %s' % tag_name)
+
+    def make_xpath(self, tag, **kwargs):
+        if kwargs == {}:
+            return ".//%s" % tag
+        else:
+            attrs = ""
+            for key in kwargs.keys():
+                attr = "@%s='%s'," % (key, kwargs[key])
+                attrs = "%s%s" % (attrs, attr)
+        
+            return ".//%s[%s]" % (tag, attrs[:-1])
---
+++ html_spec_test.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from html_spec import HtmlSpec
+from tag_exceptions import DoesNotHaveTagException
+
+HTML = """
+        <body>
+        <p>
+        <strong>Meu <a href=''>teste</a></strong>
+        <span>outro marcador</span>
+        </p>
+        <h2>Nao</h2>
+        </body>
+"""
+
+def test_make_xpath():
+    html = "<body><p><strong>Meu teste</strong></p></body>"
+    tag = "p"
+    
+    html_func = HtmlSpec(html)
+    resp = html_func.make_xpath(tag)
+    
+    assert resp == ".//p"
+
+def test_should_be_instance_html_functional():
+    v = HtmlSpec(html='<teste />')
+    assert isinstance(v, HtmlSpec) == True
+
+def test_should_raise_exception_when_not_have_tag():
+    html = '<body><h1>Titulo</h1></body>'
+    html_func = HtmlSpec(html)
+    try:
+        html_func.has_tag('h3')
+        assert False
+    except DoesNotHaveTagException, e:
+        got = e.message
+        expected = 'Html does not have tag h3'
+        assert expected == got, "\nexpected: %s \ngot: %s" % (expected, got)
+
+# Deve retornar uma nova instancia da classe HtmlSpec contendo como
+# parent o xpath usado anteriormente para buscar a tag html
+def test_should_be_return_new_instance_in_have_tag():
+    html = "<body><p>testando</p></body>"
+    html_func = HtmlSpec(html)
+    resp = html_func.has_tag('p')
+
+    assert isinstance(resp, HtmlSpec) == True
+    assert resp.node.text == "testando"
+
+def test_should_be_return_new_instance_in_have_tag_with_find_child():
+    html = "<body><p><strong>Meu teste</strong></p><strong>Nao</strong></body>"
+    html_func = HtmlSpec(html)
+    resp = html_func.has_tag('p').has_tag('strong')
+		
+    assert isinstance(resp, HtmlSpec) == True
+    assert resp.node.text != "Nao"
+    assert resp.node.text == "Meu teste"
+
+def test_should_be_return_new_instance_in_have_tag_with_find_some_level():
+    html_func = HtmlSpec(HTML)
+    resp = html_func.with_tag('p').with_tag('h2')
+    
+    html_func.has_tag('p').with_tag('strong').with_tag('span')
+		
+    assert isinstance(resp, HtmlSpec) == True
+    assert True
+
+def test_should_be_find_tag_with_attributes():
+    html = """
+        <body><div>Nao pode peguar esse</div><div id='test_id'><strong>test with attr</strong></div></body>
+    """
+    spec = HtmlSpec(html)
+    resp = spec.has_tag('div', id='test_id').has_tag('strong')
+    assert resp.node.text == 'test with attr'
+
+def test_make_xpath_with_kwargs():
+    spec = HtmlSpec(HTML)
+    xpath = spec.make_xpath('div',id='tag_id')
+    assert xpath == ".//div[@id='tag_id']"
